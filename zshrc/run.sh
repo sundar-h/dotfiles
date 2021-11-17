b587a90363418a0093fd578196d647b8b21d23b2
sanghaifa <sanghaifa@sensetime.com>
2021-11-17 17:44:20 +0800

    2021-11-17

--- zshrc/run.sh
+++ zshrc/run.sh
@@ -5,6 +5,9 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone https://github.com/IngoMeyer441/zsh-easy-motion ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-easy-motion
 git clone https://github.com/softmoth/zsh-vim-mode ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-vim-mode
+git clone https://github.com/clvv/fasd.git
+echo "cd fasd; make install"
+
 
 
 ln -s ~/data/config/zshrc/zshrc ~/.zshrc
@@ -13,4 +16,5 @@ npm config set prefix '~/.npm-global'
 
 LVBRANCH=rolling bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/rolling/utils/installer/install.sh)
 bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/rolling/utils/installer/install-neovim-from-release)
+
 
